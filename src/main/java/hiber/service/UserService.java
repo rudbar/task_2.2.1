89abb38c95149ab0be09b77affa601df7adbfb0d
Ian Kachan <kachan.yanf@gmail.com>
2021-11-27 18:15:12 +0500

    Initial commit

---
+++ src/main/java/hiber/service/UserService.java
@@ -0,0 +1,16 @@
+package hiber.service;
+
+import hiber.model.Car;
+import hiber.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+    List<User> listUsers();
+
+    void add(Car car);
+    List<Car> listCars();
+
+    void getUserByModelAndSeries(Car car);
+}
